5dbcc06613044171519a76aec74d500cdcc8e9d2
Jamefrus <jamefrus@ya.ru>
2016-04-24 18:16:32 +0300

    Refactoring

--- src/main/kotlin/su/jfdev/spek/hybrid/SpekHybrid.kt
+++ src/main/kotlin/su/jfdev/spek/hybrid/SpekHybrid.kt
@@ -2,11 +2,9 @@ package su.jfdev.spek.hybrid
 
 import org.jetbrains.spek.api.DescribeBody
 import org.jetbrains.spek.api.DescribeParser
-import org.jetbrains.spek.api.SpekTree
 import org.jetbrains.spek.junit.JUnitDescriptionCache
 import org.jetbrains.spek.junit.JUnitNotifier
 import org.junit.Test
-import org.junit.runner.Description
 import org.junit.runner.Runner
 import org.junit.runner.notification.RunNotifier
 import kotlin.reflect.KFunction
@@ -20,64 +18,57 @@ import kotlin.reflect.jvm.kotlinFunction
  */
 
 class SpekHybrid(val specificationClass: Class<*>): Runner() {
-    private val junitDescriptionCache = JUnitDescriptionCache()
-    var tree: SpekTree
-
-    init {
-        val objectInstance = objectInstance()
-        val spekFun = specFunctions()
-        val parentDescribeBody = DescribeParser()
-        parentDescribeBody.describe(specificationClass.getAnnotation(Spec::class.java)?.nullableName ?: specificationClass.simpleName){
-            spekFun.forEach {
-                buildBody(it, objectInstance)
-            }
-        }
-        tree = parentDescribeBody.children()[0]
-
-    }
-
-    private fun objectInstance(): Any? {
-        return try {
+    private val tree = let {
+        val objectInstance = try {
             specificationClass.kotlin.objectInstance
         } catch(e: KotlinReflectionInternalError) {
             null
         }
+        val description = specificationClass.getAnnotation(Spec::class.java)?.nullableName ?: specificationClass.simpleName
+        DescribeParser().run {
+            describe(description) {
+                functions().forEach {
+                    buildBody(it, objectInstance)
+                }
+            }
+            children().first()
+        }
     }
 
-    private fun specFunctions(): List<Pair<KFunction<*>, Spec?>> {
+    fun functions(): List<KFunction<*>> {
         return specificationClass.methods.filter {
             it.annotations.any { it is Test || it is Spec }
         }.map {
-            val specAnnotation = it.annotations.filterIsInstance<Spec>().firstOrNull()
-            it.kotlinFunction?.to(specAnnotation)
+            it.kotlinFunction
         }.filterNotNull()
     }
 
-    private fun DescribeBody.buildBody(it: Pair<KFunction<*>, Spec?>, objectInstance: Any?) {
-        val (function, spec) = it
-        val invokeFunc: (DescribeBody) -> Unit = { body ->
+    private fun DescribeBody.buildBody(function: KFunction<*>, objectInstance: Any?) {
+        fun invokeFunc(body: DescribeBody) {
             when (function.parameters.size) {
                 0 -> function.call()
                 1 -> function.call(if (function.parameters.first().kind == INSTANCE) objectInstance else body)
                 2 -> function.call(objectInstance, body)
             }
         }
-        val isDescribe = function.parameters.any { it.type.javaType == DescribeBody::class.java }
-        val description = spec?.nullableName ?: function.name
-        if (isDescribe) describe(description) {
-            invokeFunc(this@describe)
+
+        val description = function.annotations.filterIsInstance<Spec>().firstOrNull()?.nullableName ?: function.name
+        if (function.isDescribe()) describe(description) {
+            invokeFunc(this)
         } else it(description) {
             invokeFunc(this)
         }
     }
 
-    override fun getDescription(): Description? {
-        return junitDescriptionCache.get(tree)
-    }
 
-    override fun run(junitNotifier: RunNotifier?) {
-        tree.run(JUnitNotifier(junitNotifier!!, junitDescriptionCache))
-    }
+    private fun KFunction<*>.isDescribe() = parameters.any { it.type.javaType == DescribeBody::class.java }
+
+    private val junitDescriptionCache = JUnitDescriptionCache()
+
+    override fun getDescription() = junitDescriptionCache.get(tree)
+
+    override fun run(junitNotifier: RunNotifier) = tree.run(JUnitNotifier(junitNotifier, junitDescriptionCache))
+
 }
 
 @Retention(AnnotationRetention.RUNTIME)
